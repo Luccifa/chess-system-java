ba25961efc1271ca0f44bf19fa4dcf2b3ec99f08
Fabio Akira Lucci <fabiolucci@terra.com.br>
2021-03-16 23:44:47 -0300

    Possible move of a piece

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -1,6 +1,6 @@
 package boardgame;
 
-public class Piece {
+public abstract class Piece {
 
 	// protected => somente classes do mesmo pacote podem acessar
 	protected Position position;
@@ -13,6 +13,24 @@ public class Piece {
 
 	protected Board getBoard() {
 		return board;
+	}
+
+	public abstract boolean[][] possibleMoves();
+
+	public boolean possivleMove(Position position) {
+		return possibleMoves()[position.getRow()][position.getColumn()];
+	}
+
+	public boolean isThereAnyPossibleMove() {
+		boolean[][] math = possibleMoves();
+		for (int i = 0; i < math.length; i++) {
+			for (int j = 0; j < math.length; j++) {
+				if (math[i][j]) {
+					return true;
+				}
+			}
+		}
+		return false;
 	}
 
 }
